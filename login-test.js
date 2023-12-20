ec3b06e467ffe2d1fc1a9a99788babbbc47ff4e1
VimansaWickramasinghe <vwickramasinghe1998@gmail.com>
2023-12-20 13:57:37 +0530

    Test script for authentication

---
+++ login-test.js
@@ -0,0 +1,27 @@
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await authenticate(page);
+  await browser.close();
+})();
+
+async function authenticate(page) {
+  await page.goto("https://bitheap.tech");
+  await page.click("#menu-item-1311");
+  await page
+    .locator("[name='xoo-el-username']")
+    .fill(process.env.BITHEAP_USERNAME);
+  await page.locator("[name='xoo-el-password']").fill(process.env.PASS);
+  await page
+    .locator(
+      "xpath=/html/body/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/form/button"
+    )
+    .click();
+  const text = await page.locator("css=#menu-item-1314 > a").textContent();
+  if (text != "Hello, Playwright")
+    console.error("The authentication was not successful!");
+  await page.screenshot({ path: "screenshot2.png" });
+}
