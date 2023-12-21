905323d36ce15618864138253c0f29f5e6dba3c7
VimansaWickramasinghe <vwickramasinghe1998@gmail.com>
2023-12-21 13:48:01 +0530

    configurations and reports added

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+const config = {
+  use: {
+    headless: false,
+  },
+};
+
+module.exports = config;
+/*to see what playwright is doing while runnding the tests. 
+In the headless mode, the browser is not shown
+In default mode, playwright configs with headless mode */
